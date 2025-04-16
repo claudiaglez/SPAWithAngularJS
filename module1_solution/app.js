90137b46afcaebbafae571bca27c58876813e150
claudiaglez <claudia.glezg@gmail.com>
2025-04-16 15:52:25 +0200

    Add module 1 assignment solution

---
+++ module1_solution/app.js
@@ -0,0 +1,28 @@
+
+(function () {
+      'use strict';
+      
+      angular.module('LunchCheck', [])
+      .controller('LunchCheckController', LunchCheckController);
+      
+      LunchCheckController.$inject = ['$scope'];
+      function LunchCheckController($scope) {
+        $scope.lunchMenu = "";
+        $scope.message = "";
+    
+        $scope.checkIfTooMuch = function () {
+          if ($scope.lunchMenu.trim() === "") {
+            $scope.message = "Please enter data first";
+            return;
+          }
+    
+          var dishes = $scope.lunchMenu.split(',');
+          if (dishes.length <= 3) {
+            $scope.message = "Enjoy!";
+          } else {
+            $scope.message = "Too much!";
+          }
+        };
+      }
+    })();
+    
