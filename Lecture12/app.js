0054e6797c541266afaf466d96e4263bb56b50db
claudiaglez <claudia.glezg@gmail.com>
2025-04-21 09:35:24 +0200

    Add Lecture 12: Filters

--- Lecture12/app.js
+++ Lecture12/app.js
@@ -4,13 +4,16 @@
   angular.module('MsgApp', [])
   .controller('MsgController', MsgController);
   
-  MsgController.$inject = ['$scope',];
-  function MsgController($scope) {
+  MsgController.$inject = ['$scope', '$filter'];
+  function MsgController($scope, $filter) {
     $scope.name = "Claudia";
     $scope.stateOfBeing = "hungry";
+    $scope.cookieCost = .45;
 
     $scope.sayMessage = function () {
-      return "Claudia wants to learn AngularJS"
+      var msg = "Claudia wants to learn AngularJS";
+      var output = $filter('uppercase')(msg);
+      return output;
     };
 
     $scope.feedPeople = function () {
--- module1_solution/app.js
+++ module1_solution/app.js
@@ -1,28 +1,25 @@
-
 (function () {
-      'use strict';
-      
-      angular.module('LunchCheck', [])
-      .controller('LunchCheckController', LunchCheckController);
-      
-      LunchCheckController.$inject = ['$scope'];
-      function LunchCheckController($scope) {
-        $scope.lunchMenu = "";
-        $scope.message = "";
-    
-        $scope.checkIfTooMuch = function () {
-          if ($scope.lunchMenu.trim() === "") {
-            $scope.message = "Please enter data first";
-            return;
-          }
-    
-          var dishes = $scope.lunchMenu.split(',');
-          if (dishes.length <= 3) {
-            $scope.message = "Enjoy!";
-          } else {
-            $scope.message = "Too much!";
-          }
-        };
-      }
-    })();
-    
+  "use strict";
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
+  LunchCheckController.$inject = ["$scope"];
+  function LunchCheckController($scope) {
+    $scope.lunchMenu = "";
+    $scope.message = "";
+
+    $scope.checkIfTooMuch = function () {
+      if ($scope.lunchMenu.trim() === "") {
+        $scope.message = "Please enter data first";
+        return;
+      }
+
+      var dishes = $scope.lunchMenu.split(",");
+      if (dishes.length <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    };
+  }
+})();
