33d8b942318c58dd5b20f6efb1dbab9c18820acc
claudiaglez <claudia.glezg@gmail.com>
2025-04-21 12:54:13 +0200

    Lecture 14: Digest Cycle, part 2

--- Lecture14/app.js
+++ Lecture14/app.js
@@ -8,6 +8,7 @@
   function CounterController($scope) {
     $scope.onceCounter = 0;
     $scope.counter = 0;
+    $scope.name = "Claudia";
   
     $scope.showNumberOfWatchers = function () {
       console.log("· of watchers: ", $scope.$$watchersCount);
@@ -22,15 +23,19 @@
       $scope.counter++;
     };
 
-    $scope.$watch('onceCounter', function (newValue, oldValue) {
-      console.log("onceCounter old value: ", oldValue);
-      console.log("onceCounter new value: ", newValue);
+    $scope.$watch(function () {
+      console.log("Digest Loop Fired!");
     })
 
-    $scope.$watch('counter', function (newValue, oldValue) {
-      console.log("counter old value: ", oldValue);
-      console.log("counter new value: ", newValue);
-    })
+    // $scope.$watch('onceCounter', function (newValue, oldValue) {
+    //   console.log("onceCounter old value: ", oldValue);
+    //   console.log("onceCounter new value: ", newValue);
+    // })
+
+    // $scope.$watch('counter', function (newValue, oldValue) {
+    //   console.log("counter old value: ", oldValue);
+    //   console.log("counter new value: ", newValue);
+    // })
   
   }
   
